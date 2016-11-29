3acf9fc7915c7fea2c647ff681a196c25017df2e
Pierre Donat-Bouillud <pierre.donat-bouillud@ircam.fr>
2016-11-29 16:18:26 +0100

    Better dot representation

--- src/main.ml
+++ src/main.ml
@@ -50,12 +50,20 @@ module G = Imperative.Digraph.ConcreteLabeled(Node)(Edge)
 
 let remove_quotes = String.replace_chars (fun c -> if c = '\"' then "" else Char.escaped c)
 
+
 module Dot = Graphviz.Dot(struct
     include G
     let edge_attributes e = [] (*let open Graphviz.DotAttributes in  [`Label (string_of_int e)] *)
     let default_edge_attributes _ = []
     let get_subgraph _ = None
-    let vertex_attributes v = [`Shape `Box ; `Label  (if v.className = "newobj" then remove_quotes (Option.default "newobj"  v.text) else v.className) ]
+    let vertex_attributes v = (* [`Shape `Box; `Label (if v.className == "newobj" then remove_quotes (Option.default "newobj"  v.text) else v.className)] *)
+      (`Shape `Box) ::
+        match v.className with
+          | "newobj" -> [`Label (remove_quotes (Option.default "newobj"  v.text))]
+          | "comment" -> [`Label (remove_quotes (Option.get v.text))]
+          | "message" -> [`Label (Option.get v.text)]
+          | _ -> [`Label v.className]
+
     let vertex_name v = "\"" ^ v.id ^ "\""
     let default_vertex_attributes _ = []
     let graph_attributes _ = []
