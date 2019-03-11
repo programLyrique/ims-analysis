f536c881277c3ed72c7ced1f240c7659114029b9
Pierre Donat-Bouillud <pierre.donat-bouillud@ircam.fr>
2019-03-11 14:49:14 +0100

    Another graph generation method, with nb vertices and prob of having an edge.

--- src/random_graph.ml
+++ src/random_graph.ml
@@ -2,8 +2,9 @@
 open Batteries
 open Flowgraph
 
-(* See https://stackoverflow.com/questions/12790337/generating-a-random-dag *)
-let gen_dag nb_vertices nb_edges =
+(* See https://stackoverflow.com/questions/12790337/generating-a-random-dag
+   Generate a Dag goven a number of edges and a number of vertices *)
+let gen_dag_v_e nb_vertices nb_edges =
   let n = nb_vertices * (nb_vertices - 1) / 2 in
   assert (0 < nb_edges);
   assert (nb_edges < n);
@@ -26,4 +27,22 @@ let gen_dag nb_vertices nb_edges =
       let dst = endpoints.(head) in
       G.add_edge_e graph (G.E.create vertices.(src) (1,1) vertices.(dst))
     ) sample;
+  graph
+
+(* Generate a Dag given a number of nodes and a probability of having an edge between two vertices *)
+let  gen_dag_v nb_vertices p =
+  (*So that we don't have the same ids... Do we actually really care as node generation will be done later? *)
+  let endpoints = Enum.range ~until:(nb_vertices - 1) 0 in
+  let endpoints = Random.shuffle endpoints in
+  (*Get the actual nodes*)
+  let vertices = Array.init nb_vertices (fun i -> G.V.create (Node.make ("id-" ^ string_of_int endpoints.(i) ) 1 1 "plop")) in
+  let graph = G.create ~size:nb_vertices () in
+  Array.iter (fun v -> G.add_vertex graph v) vertices;
+  let one = 1. +. epsilon_float in (*We want an uniform distribution in the close range [0,1]*)
+  for i = 0 to nb_vertices - 1 do
+    for j = 0 to i - 1 do
+      if Random.float one <= p then
+        G.add_edge_e graph (G.E.create vertices.(i) (1,1) vertices.(j))
+    done
+  done;
   graph
--- tests/random_tests.ml
+++ tests/random_tests.ml
@@ -4,12 +4,20 @@ open Batteries
 open Flowgraph
 
 
-let random_graph test_ctxt =
+let random_graph_v_e test_ctxt =
   Random.init 678;
   let nb_vertices = 30 and nb_edges = 40 in
-  let graph = Random_graph.gen_dag nb_vertices nb_edges in
+  let graph = Random_graph.gen_dag_v_e nb_vertices nb_edges in
   assert_equal  ~printer:string_of_int nb_vertices (G.nb_vertex graph);
   assert_equal  ~printer:string_of_int nb_edges (G.nb_edges graph)
 
+let random_graph_v test_ctxt =
+  Random.init 236;
+  let nb_vertices = 60  in
+  let p = 0.3 in
+  let graph = Random_graph.gen_dag_v nb_vertices p in
+  assert_equal  ~printer:string_of_int nb_vertices (G.nb_vertex graph)
 
-  let suite = "random graph" >::: ["random_graph" >:: random_graph]
+
+  let suite = "random graph" >::: ["random_graph_v_e" >:: random_graph_v_e;
+                                   "random_graph_v" >:: random_graph_v]
